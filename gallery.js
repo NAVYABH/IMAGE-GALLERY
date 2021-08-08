033478037861f2421832d16da9794f5d1fefc1a9
NAVYABH <76563220+NAVYABH@users.noreply.github.com>
2021-08-08 12:56:50 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,33 @@
+function openWin1() {
+    window.open("./images/2.jpg");
+}
+function openWin2() {
+    window.open("./images/3.jpg");
+}
+function openWin3() {
+    window.open("./images/1.jpg");
+}
+function openWin4() {
+    window.open("./images/6.jpg");
+}
+function openWin5() {
+    window.open("./images/4.jpg");
+}
+function openWin6() {
+    window.open("./images/5.jpg");
+}
+function openWin7() {
+    window.open("./images/7.jpg");
+}
+function openWin8() {
+    window.open("mission.html");
+}
+
+function openWin9() {
+    window.open("vision.html");
+}
+function openWin10(){
+    window.open("team.html");
+}
+
+
